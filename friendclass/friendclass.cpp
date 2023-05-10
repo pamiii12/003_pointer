f94a404e47374f4b87287d08d574e4c974ce4c12
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-10 15:31:57 +0700

    final (FriendClass)

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -22,3 +22,25 @@ public:
 
 };
 
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+void siswa::setld(int pId) {
+	id = pId;
+}
+
+int main() {
+	orang joko;
+	joko.setNama("Joko Kumat");
+	siswa joko_siswa;
+	joko_siswa.setld(1);
+	joko_siswa.displayAll(joko);
+
+	return 0;
+}
+
